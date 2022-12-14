9d345cd211e5b823834af7344854bea8bbbea5ba
Angelo31294 <luongoangelo1@gmail.com>
2022-12-14 15:55:25 +0100

    Agg. Stile tabella

--- index.php
+++ index.php
@@ -40,20 +40,43 @@
 
     ];
 
-    var_dump($hotels);
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Hotel</title>
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Hotel</title>
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    </head>
+    <body>
+
+        <table class="table table-success table-striped">
+            <thead>
+                <tr>
+                <th scope="col"></th>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Voto</th>
+                <th scope="col">Distanza dal centro (km)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php for( $i = 0; $i < count($hotels); $i++) { ?>
+                    <tr>
+                    <th scope="row"></th>
+                    <td><?php echo $hotels[$i]['name']?></td>
+                    <td><?php echo $hotels[$i]['description']?></td>
+                    <td><?php echo $hotels[$i]['parking']?></td>
+                    <td><?php echo $hotels[$i]['vote']?></td>
+                    <td><?php echo $hotels[$i]['distance_to_center']?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+        
+    </body>
 </html>
